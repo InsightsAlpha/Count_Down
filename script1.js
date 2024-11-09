026ca1fb2c84aecf48618853a575fee3bfc36e90
SATYAM KUMAR <95396644+InsightsAlpha@users.noreply.github.com>
2024-11-09 13:28:31 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 let countdownInterval;
 
 // Set the target date and time (YYYY-MM-DDTHH:MM:SS)
- const targetDate = new Date("2024-12-18T00:00:00").getTime(); // Set target date to 18th December 2024 at 00:00:00
+const targetDate = new Date("2024-12-18T00:00:00").getTime(); // Set target date to 18th December 2024 at 00:00:00
 
 // Set the target date and time to 1 minute from now
 //const targetDate = new Date(Date.now() + 1 * 60 * 1000).getTime(); // Set target date to 1 minute from now
@@ -26,11 +26,13 @@ function startCountdown() {
 
         // If the countdown is finished, redirect to the wishes.html page
         if (distance = 0) {
-            clearInterval(countdownInterval);
+     
             window.location.href = "https://insightsalpha.github.io/BirthaDay_Wishes/"; // Redirect to the wishes page
             // document.getElementById('message').innerHTML = "Countdown finished!";
+           clearInterval(countdownInterval);
+         
         }
-    }, 100);
+    }, 1000);
 }
 
 // Automatically start the countdown when the page loads
